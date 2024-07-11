bdc1e4130053551410d94e052a00af43f6a47ca1
mhlehri <lehrimirza101@gmail.com>
2024-07-11 10:54:55 +0600

    add: add product controller

--- src/app/modules/products/products.controller.ts
+++ src/app/modules/products/products.controller.ts
@@ -1,7 +1,20 @@
 import { RequestHandler } from "express";
 import { createProductIntoDB } from "./products.service";
 
-export const addProduct: RequestHandler = async (req, res) => {};
+export const addProduct: RequestHandler = async (req, res) => {
+  try {
+    await createProductIntoDB(req.body);
+    res.json({
+      success: true,
+      message: "Product added successfully!",
+    });
+  } catch (error: any) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 export const getProducts: RequestHandler = () => {};
 export const getProductById: RequestHandler = () => {};
 export const updateProduct: RequestHandler = () => {};
